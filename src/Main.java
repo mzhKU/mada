2402a06a56569e9fae3fc87720853ea0376cecc1
Martin Hediger <ma.hed@bluewin.ch>
2019-10-15 19:46:17 +0200

    Restore printing of original task secret message
    This reverts commit 8c651580d6f2f2e4dd1dc272b3d0a8742eb50d9a.

--- src/Main.java
+++ src/Main.java
@@ -8,8 +8,6 @@ public class Main {
 
     private static boolean fast = false;
     private static boolean log = true;
-    private static final String SECRET_KEY = "my-sk.txt";
-    private static final String PUBLIC_KEY = "my-pk.txt";
 
     // Questions
     // - How to determine 'e'? Is it OK like this?
@@ -54,14 +52,14 @@ public class Main {
 
         // Write secret key.
         try {
-            Writer.writeKey(SECRET_KEY, rsa.getN(), d);
+            Writer.writeKey("my-sk.txt", rsa.getN(), d);
         }  catch (IOException ex){
             System.out.println("Key not written.");
         }
 
         // Write private key.
         try {
-            Writer.writeKey(PUBLIC_KEY, rsa.getN(), e);
+            Writer.writeKey("my-pk.txt", rsa.getN(), e);
         } catch (IOException ex){
             System.out.println("Key not written.");
         }
